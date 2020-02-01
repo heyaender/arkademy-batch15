90cc65fe80e33a00e7f8fd57e2ddc5f44941ea79
thecakto <putroandrianto@icloud.com>
2020-02-01 15:55:28 +0700

    soal 1 done

--- 1.py
+++ 1.py
@@ -1,6 +1,22 @@
+#coba run ke python
+
 import json
 
-soal_1 = open('1.json')
-data = json.loads(soal_1.read())
+with open('1.json') as data:
 
-print data
+    bdata = json.loads(data.read())
+
+    print "Nama \t: %s" % bdata['nama']
+    print "Umur \t: %s" % bdata['umur']
+    print "Alamat\t: %s" % bdata['alamat']
+
+    print "Hobi\t:"
+    for hobi in bdata['hobi']:
+        print "\t %s" % (hobi)
+
+    print "Pendidikan\t:"
+    for pend in bdata['riwayatPendidikan']:
+        print "\t %s" % (pend['namaSekolah'])
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # arkademy-batch15
+
+
+JSON (JavaScript Object Notation) itu sebuah format data yang digunakan untuk pertukaran dan penyimpanan data, intinya seperti itu. 
+jika JSON dalam RestAPI itu untuk betukar data client dan server atau antar aplikasi, jadi contoh semisal data dari aplikasi bukalapak bisa sama dengan data di web bukalapak.
+ya dengan RESTful API tadi itu, jadi JSON menyiapkan data yang siap dikonsumsi atau digunakan oleh client (aplikasi lainnya).
